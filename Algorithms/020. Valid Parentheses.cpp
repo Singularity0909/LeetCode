c95a76af246b346ca184ad4993d3e8cae084a342
Singularity0909 <904854724@qq.com>
2020-04-10 10:18:06 +0800

    update

---
+++ Algorithms/020. Valid Parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        char a[128];
+        a[')'] = '(';
+        a[']'] = '[';
+        a['}'] = '{';
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                st.push(s[i]);
+            }
+            else if (!st.empty() && st.top() == a[s[i]]) {
+                st.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        return st.empty();
+    }
+};

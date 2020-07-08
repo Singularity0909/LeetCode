de240ad78314f6607df86d4d5a0b6066d62a730e
Singularity0909 <904854724@qq.com>
2020-07-08 22:16:24 +0800

    Update 2020-07-08

---
+++ Problemset/diving-board-lcci/diving-board-lcci.cpp
@@ -0,0 +1,18 @@
+
+// @Title: 跳水板 (Diving Board LCCI)
+// @Author: Singularity0909
+// @Date: 2020-07-08 00:21:30
+// @Runtime: 28 ms
+// @Memory: 20.3 MB
+
+class Solution {
+public:
+    vector<int> divingBoard(int shorter, int longer, int k) {
+        if (!k) return {};
+        if (shorter == longer) return { shorter * k };
+        vector<int> ans;
+        for (int i = 0; i <= k; i++)
+            ans.push_back(shorter * (k - i) + longer * i);
+        return ans;
+    }
+};

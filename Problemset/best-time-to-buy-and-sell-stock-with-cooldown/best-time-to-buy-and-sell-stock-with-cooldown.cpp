1fa72e87264b72c24dc67c410dc42830a5c9aef8
Singularity0909 <904854724@qq.com>
2020-07-10 17:25:10 +0800

    Update 2020-07-10

---
+++ Problemset/best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,22 @@
+
+// @Title: 最佳买卖股票时机含冷冻期 (Best Time to Buy and Sell Stock with Cooldown)
+// @Author: Singularity0909
+// @Date: 2020-07-10 15:59:00
+// @Runtime: 8 ms
+// @Memory: 12.1 MB
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) return 0;
+        size_t n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(3, 0));
+        dp[0][0] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] - prices[i]);
+            dp[i][1] = dp[i - 1][0] + prices[i];
+            dp[i][2] = max(dp[i - 1][1], dp[i - 1][2]);
+        }
+        return max(dp[n - 1][1], dp[n - 1][2]);
+    }
+};
---
+++ Problemset/re-space-lcci/re-space-lcci.cpp
@@ -0,0 +1,62 @@
+
+// @Title: 恢复空格 (Re-Space LCCI)
+// @Author: Singularity0909
+// @Date: 2020-07-09 23:45:52
+// @Runtime: 116 ms
+// @Memory: 154.4 MB
+
+class Trie {
+public:
+    Trie* next[26] = {nullptr};
+    bool isEnd;
+    
+    Trie() {
+        isEnd = false;
+    }
+
+    void insert(string s) {
+        Trie* curPos = this;
+
+        for (int i = s.length() - 1; i >= 0; --i) {
+            int t = s[i] - 'a';
+            if (curPos->next[t] == nullptr) {
+                curPos->next[t] = new Trie();
+            }
+            curPos = curPos->next[t];
+        }
+        curPos->isEnd = true;
+    }
+};
+
+class Solution {
+public:
+    int respace(vector<string>& dictionary, string sentence) {
+        int n = sentence.length(), inf = 0x3f3f3f3f;
+
+        Trie* root = new Trie();
+        for (auto& word: dictionary) {
+            root->insert(word);
+        }
+
+        vector<int> dp(n + 1, inf);
+        dp[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            dp[i] = dp[i - 1] + 1;
+
+            Trie* curPos = root;
+            for (int j = i; j >= 1; --j) {
+                int t = sentence[j - 1] - 'a';
+                if (curPos->next[t] == nullptr) {
+                    break;
+                } else if (curPos->next[t]->isEnd) {
+                    dp[i] = min(dp[i], dp[j - 1]);
+                }
+                if (dp[i] == 0) {
+                    break;
+                }
+                curPos = curPos->next[t];
+            }
+        }
+        return dp[n];
+    }
+};

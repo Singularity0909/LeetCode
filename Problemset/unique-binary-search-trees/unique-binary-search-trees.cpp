b0318a5119d386871f13cf7468025425d10a02a2
Singularity0909 <904854724@qq.com>
2020-07-15 02:01:36 +0800

    Update 2020-07-15

---
+++ Problemset/unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,25 @@
+
+// @Title: 不同的二叉搜索树 (Unique Binary Search Trees)
+// @Author: Singularity0909
+// @Date: 2020-07-15 02:00:29
+// @Runtime: 0 ms
+// @Memory: 6.3 MB
+
+class Solution {
+public:
+    vector<int> res;
+
+    int dfs(int n) {
+        if (res[n]) return res[n];
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+            sum += dfs(i - 1) * dfs(n - i);
+        return res[n] = sum;
+    }
+
+    int numTrees(int n) {
+        res.resize(n + 1);
+        res[0] = 1;
+        return dfs(n);
+    }
+};

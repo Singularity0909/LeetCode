233969f360c02268c49bc199e5c97201f9fc5b8b
Singularity0909 <904854724@qq.com>
2020-07-24 00:43:54 +0800

    Update 2020-07-24

---
+++ Problemset/divisor-game/divisor-game.cpp
@@ -0,0 +1,14 @@
+
+// @Title: 除数博弈 (Divisor Game)
+// @Author: Singularity0909
+// @Date: 2020-07-24 00:42:21
+// @Runtime: 0 ms
+// @Memory: 5.7 MB
+
+class Solution {
+public:
+    bool divisorGame(int N)
+    {
+        return N % 2 == 0;
+    }
+};

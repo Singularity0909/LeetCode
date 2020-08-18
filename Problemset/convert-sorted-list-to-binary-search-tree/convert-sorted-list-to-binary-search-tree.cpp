4ef82d191e775bdbee528bfaa79dd7dd7406ba05
Singularity0909 <904854724@qq.com>
2020-08-18 22:38:02 +0800

    Update 2020-08-18

---
+++ Problemset/convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,33 @@
+
+// @Title: 有序链表转换二叉搜索树 (Convert Sorted List to Binary Search Tree)
+// @Author: Singularity0909
+// @Date: 2020-08-18 22:34:58
+// @Runtime: 24 ms
+// @Memory: 24.4 MB
+
+class Solution {
+public:
+    int getLength(ListNode* head) {
+        int ret = 0;
+        for (; head != nullptr; ++ret, head = head->next);
+        return ret;
+    }
+
+    TreeNode* buildTree(ListNode*& head, int left, int right) {
+        if (left > right) {
+            return nullptr;
+        }
+        int mid = (left + right + 1) / 2;
+        TreeNode* root = new TreeNode();
+        root->left = buildTree(head, left, mid - 1);
+        root->val = head->val;
+        head = head->next;
+        root->right = buildTree(head, mid + 1, right);
+        return root;
+    }
+
+    TreeNode* sortedListToBST(ListNode* head) {
+        int length = getLength(head);
+        return buildTree(head, 0, length - 1);
+    }
+};
